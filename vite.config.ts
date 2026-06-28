59db1eea3d7ac0f169b733ac322fe9ef7b203948
Harish Jangam <jangamharish949@gmail.com>
2026-06-28 19:02:46 +0530

    updating

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,21 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
-  server: {
-    host: "::",
-    port: 8081,
-  },
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+// export default defineConfig(() => ({
+//   server: {
+//     host: "::",
+//     port: 8081,
+//   },
+//   plugins: [
+//     react(),
+//   ],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
+export default defineConfig({
+  base: '/portfolio.io/', // Add this line
+  plugins: [react()], // or whatever plugins you have
+})
